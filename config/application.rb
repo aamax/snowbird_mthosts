05147acd58eaa58e58eaba6e880fd0f2280ab131
Allen Maxwell <aamax@xmission.com>
2013-10-07 19:54:54 -0600

    add precompile setting to application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Mthost
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.assets.initialize_on_precompile = false
+
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
