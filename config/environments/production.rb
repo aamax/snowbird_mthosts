534333eca2a4efcf8bcc876b10a19be0e0ec6f3c
Allen Maxwell <aamax@xmission.com>
2013-11-19 16:42:34 -0700

    set logger for production for unicorn as per heroku example

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,9 @@ Mthost::Application.configure do
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  # Set logger to STDOUT for Unicorn logging
+  config.logger = Logger.new(STDOUT)
+
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
