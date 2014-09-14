5fe3f60b07fcfd4a344a4a34a5b32ea79b4c0da5
Allen Maxwell <aamax@xmission.com>
2014-09-14 13:52:07 -0600

    update dates for 2014 season

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,10 +1,10 @@
 DAYNAMES = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 
-MEETINGS = [{:who=>:rookie, :when=>"2013-11-2", :time=>"8:00am",:what=>"Rookie Training Meeting", :type=>"M1"},
-            {:who=>:all, :when=>"2013-11-16", :time=>"8:00am", :what=>"All Host Training", :type=>"M2"},
-            {:who=>:rookie, :when=>"2013-12-15", :time=>"8:00am", :what=>"On Mountain Training", :type=>"M3"},
-            {:who=>:all, :when=>"2014-1-25", :time=>"4:00pm", :what=>"Mid-Year Meeting", :type=>"M4"}]
+MEETINGS = [{:who=>:rookie, :when=>"2014-11-1", :time=>"8:00am",:what=>"Rookie Training Meeting", :type=>"M1"},
+            {:who=>:all, :when=>"2014-11-15", :time=>"8:00am", :what=>"All Host Training", :type=>"M2"},
+            {:who=>:rookie, :when=>"2014-12-14", :time=>"8:00am", :what=>"On Mountain Training", :type=>"M3"},
+            {:who=>:all, :when=>"2015-1-24", :time=>"4:00pm", :what=>"Mid-Year Meeting", :type=>"M4"}]
 
-HOLIDAYS = [Date.new(2013,12,24),Date.new(2013,12,25),Date.new(2013,12,31),Date.new(2014,1,1)]
+HOLIDAYS = [Date.new(2014,12,24),Date.new(2014,12,25),Date.new(2014,12,31),Date.new(2015,1,1)]
 
 MAX_EMAIL = 'aamaxworks@gmail.com'
