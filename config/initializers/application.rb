f6a7940ce7cb1622d6d7c64762119c2c1c950895
Allen Maxwell <aamax@xmission.com>
2016-10-12 21:31:49 -0600

    add meeting schedule to bingo page

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,16 +1,16 @@
 DAYNAMES = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 
-MEETINGS = [{:who=>:rookie, :when=>"2015-10-31", :time=>"8:00am",:what=>"Rookie Training Meeting", :type=>"M1"},
-            {:who=>:all, :when=>"2015-11-7", :time=>"8:00am", :what=>"All Host Training", :type=>"M2"},
-            {:who=>:rookie, :when=>"2015-12-20", :time=>"8:00am", :what=>"On Mountain Training", :type=>"M3"},
-            {:who=>:all, :when=>"2016-1-23", :time=>"4:00pm", :what=>"Mid-Year Meeting", :type=>"M4"}]
+MEETINGS = [{:who=>:rookie, :when=>"2016-10-29", :time=>"8:00am",:what=>"Rookie Training Meeting", :type=>"M1"},
+            {:who=>:all, :when=>"2016-11-5", :time=>"8:00am", :what=>"All Host Training", :type=>"M2"},
+            {:who=>:rookie, :when=>"2016-12-18", :time=>"8:00am", :what=>"On Mountain Training", :type=>"M3"},
+            {:who=>:all, :when=>"2016-1-21", :time=>"4:00pm", :what=>"Mid-Year Meeting", :type=>"M4"}]
 
-HOLIDAYS = [Date.new(2015,12,24),
-            Date.new(2015,12,25),
-            Date.new(2015,12,31),
-            Date.new(2016,1,1),
-            Date.new(2016,1,18),
-            Date.new(2016,2,15)]
+HOLIDAYS = [Date.new(2016,12,24),
+            Date.new(2016,12,25),
+            Date.new(2016,12,31),
+            Date.new(2017,1,1),
+            Date.new(2017,1,16),
+            Date.new(2017,2,13)]
 
 MAX_EMAIL = 'aamaxworks@gmail.com'
 COTTER_EMAIL = 'jecotterii@gmail.com'
