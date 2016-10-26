8c954c810333ed2a803999a367d1814809bc7bbd
Allen Maxwell <aamax@xmission.com>
2016-10-26 08:55:18 -0600

    fix meeting schedule for 2016/17

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -3,7 +3,7 @@ DAYNAMES = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 MEETINGS = [{:who=>:rookie, :when=>"2016-10-29", :time=>"8:00am",:what=>"Rookie Training Meeting", :type=>"M1"},
             {:who=>:all, :when=>"2016-11-5", :time=>"8:00am", :what=>"All Host Training", :type=>"M2"},
             {:who=>:rookie, :when=>"2016-12-18", :time=>"8:00am", :what=>"On Mountain Training", :type=>"M3"},
-            {:who=>:all, :when=>"2016-1-21", :time=>"4:00pm", :what=>"Mid-Year Meeting", :type=>"M4"}]
+            {:who=>:all, :when=>"2017-1-21", :time=>"4:00pm", :what=>"Mid-Year Meeting", :type=>"M4"}]
 
 HOLIDAYS = [Date.new(2016,12,24),
             Date.new(2016,12,25),
