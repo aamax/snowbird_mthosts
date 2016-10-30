dd94b8a122f6cf9a6ef0ef297bf5d6b3c283087c
Allen Maxwell <aamax@xmission.com>
2016-10-29 19:16:58 -0600

    pre-changes setup.  ready to refactor controller and view

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -38,6 +38,7 @@ class ShiftsController < ApplicationController
       @holidays = (params['filter']['holiday_shifts'] == '1')
       @unselected = (params['filter']['show_unselected'] == '1')
       @meeting_flag = (params['filter']['show_meetings'] == '1')
+
       if @can_select == false
         @shifts = Shift.includes(:user).includes(:shift_type).from_today(@from_today).by_shift_type(@sts).by_date(@dt).by_day_of_week(@dow).by_users(@usrs).by_holidays(@holidays).by_unselected(@unselected).with_meetings(@meeting_flag).order(:shift_date, :short_name).paginate(:page => params[:page], :per_page => per_page)
       else
@@ -50,6 +51,7 @@ class ShiftsController < ApplicationController
       @can_select = true
       @shifts = Shift.includes(:user).includes(:shift_type).from_today(true).with_meetings(@meeting_flag).order(:shift_date, :short_name).to_a.delete_if {|s| s.can_select(current_user) == false }.paginate(:page => params[:page], :per_page => per_page)
     end
+
     @shifts
   end
 
