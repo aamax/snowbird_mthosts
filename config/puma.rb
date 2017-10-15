654fe3d75dd41aad857f7863aded10aeda7a60a2
= <allen.maxwell@bhghome.com>
2017-10-14 19:45:08 -0600

    remove echo from puma config

--- config/puma.rb
+++ config/puma.rb
@@ -11,8 +11,6 @@ shared_dir = "#{app_dir}/shared"
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
-echo rails_env
-
 # Set up socket location
 bind "unix://#{shared_dir}/sockets/puma.sock"
 
